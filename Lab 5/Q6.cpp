70cf6d06db22fe21288b655e61036b6246c24941
Baziluddin <rrayyantech@gmail.com>
2025-09-21 12:06:33 +0500

    Create Q6.cpp

---
+++ Lab 5/Q6.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+bool isSafe(int** arr, int x, int y, int n) 
+{
+    if (x >= 0 && x < n && y >= 0 && y < n && arr[x][y] == 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ratInMaze(int** arr, int x, int y, int n, int** solArr) 
+{
+
+    if (x == n - 1 && y == n - 1) 
+    {
+        solArr[x][y] = 1;
+        return true;
+    }
+
+    if (isSafe(arr, x, y, n) && solArr[x][y] == 0) 
+    {  
+      
+        solArr[x][y] = 1;
+
+       
+        if (ratInMaze(arr, x + 1, y, n, solArr)) 
+        {
+            return true;
+        }
+
+
+        if (ratInMaze(arr, x, y + 1, n, solArr)) 
+        {
+            return true;
+        }
+
+
+        if (ratInMaze(arr, x - 1, y, n, solArr))
+        { 
+            return true;
+        }
+
+    
+        if (ratInMaze(arr, x, y - 1, n, solArr)) 
+        {
+            return true;
+        }
+
+        solArr[x][y] = 0;
+        return false;
+    }
+    return false;
+}
+
+int main()
+{
+    int n;
+    cout << " Please Enter size of Rat maze N : ";
+    cin >> n;
+
+    int** arr = new int*[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[n];
+    }
+
+    cout << "Enter maze 0 for blocked, 1 = open): ";
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            int b;
+            cout << " Enter : " << endl;
+            cin >> b;
+            arr[i][j] = b;
+        }
+    }
+
+    int** solArr = new int*[n];
+    for (int i = 0; i < n; i++)
+    {
+        solArr[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            solArr[i][j] = 0;
+        }
+    }
+
+    if (ratInMaze(arr, 0, 0, n, solArr))
+    {
+        cout << " Successful In Path founding . ";
+        cout << endl;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+            cout << solArr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    } 
+    else 
+    {
+        cout << " No path found. ";
+    }
+    
+    for(int r =0; r< n;r++)
+    {
+        delete solArr[r];
+        delete arr[r];
+    }
+    
+    delete [] solArr;
+    delete [] arr;
+
+    return 0;
+}
